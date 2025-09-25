6c762962dcd4d2274e2e97b1dd02d81d2de7fbbf
spiers448 <LoCkEr#144>
2025-09-25 17:09:36 +0100

    Added code for calculator app

--- Week1-CalculatorApp/Program.cs
+++ Week1-CalculatorApp/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//first set of values
+Console.WriteLine("Enter first value");
+int val1 = Convert.ToInt32(Console.ReadLine());
+
+//select operator
+Console.WriteLine("Select operation (+, -, /, *)");
+char op = Convert.ToChar(Console.ReadLine());
+int answer = 0;
+
+//second set of values
+Console.WriteLine("Enter second value");
+int val2 = Convert.ToInt32(Console.ReadLine());
+
+switch (op)
+{
+    case '+': 
+      answer = val1 + val2;
+      break;
+    case '-':
+        answer = val1 - val2;
+        break;
+    case '*':
+        answer = val1 * val2;
+        break;
+    case '/':
+        answer = val1 / val2;
+        break;
+    default:
+        answer = 0;
+        break;
+
+}
+
+//print answer
+Console.WriteLine("The answer is " + answer);
